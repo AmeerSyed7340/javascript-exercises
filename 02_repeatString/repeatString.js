de2d9b078e4a0d48021bbd8dfaf27820734f04d5
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-02 04:04:59 -0400

    TDD successful for helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return `ERROR`;
+    }
+    strCopy = '';
+    for(i = 0; i < num; i++){
+        strCopy+=string;
+    }
+    return strCopy;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
