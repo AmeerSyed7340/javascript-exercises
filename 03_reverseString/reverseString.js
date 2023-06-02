9c7e7de9538ea43acefd14aabcb9aff52407b81d
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-02 05:36:41 -0400

    TDD successful for reverseString and removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseArr = Array.from(string).reverse();
+    const reverseString = reverseArr.join("");
+    return reverseString;
 };
-
+console.log(reverseString(`Hello mate`))
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    for(let number of num){
+        for(i = 0; i < array.length; i++){
+            if(array[i] === number){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
-
+console.log(removeFromArray([1,2,3,4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
