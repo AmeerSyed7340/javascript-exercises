12e3862b03c247133e99e4caf499ada2106e2d43
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-07 05:00:38 -0400

    GetTheTitles successful

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const getTitles = array.map(book => book.title);
+    return getTitles;
 };
 
 // Do not edit below this line
