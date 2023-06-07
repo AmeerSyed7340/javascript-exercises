3dbb33edd3053623b22a6f46a33460d7228b27f6
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-07 04:52:56 -0400

    Fibonacci successful

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 1) return `OOPS`;
+    else if(a == 1 || a == 2){
+        return 1;
+    }
+    else{
+        let prev = 1;
+        let prevprev = 1;
+        let result = 0;
+        for(i = 3; i <= a; i++){
+            result = prev + prevprev;
+            prevprev = prev;
+            prev = result;
+        }
+        return result;
+    }
 };
-
+console.log(fibonacci('15'));
 // Do not edit below this line
 module.exports = fibonacci;
