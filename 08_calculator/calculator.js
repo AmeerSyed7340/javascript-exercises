b02ea55c8fc0b9734081e50628e1bdf63da014e8
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-07 04:03:15 -0400

    Scucesssfully done calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sumOfArr = array.reduce((accumulator, currentVal) =>{
+    return accumulator + currentVal;
+  }, 0);
+  return sumOfArr;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const multiplyArry = array.reduce((accumulator, currentVal) =>{
+    return accumulator * currentVal;
+  }, 1);
+  return multiplyArry;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	if(b < 0) return 'power must be bigger than 0';
+  else if (b === 1) return a;
+  else{    
+    let num = 1;
+    for(i = 0; i < b; i++){
+      console.log(num);
+      num*=a;      
+    }
+    return num;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) return 1;
+  else if( a === 1 || a === 2){
+    return a;
+  }
+  else{
+    let fact = 1;
+    for(i = a; i > 0; i--){
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
