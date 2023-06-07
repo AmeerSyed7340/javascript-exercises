f99e0676ac653e130bfbae822c32113639ea42dc
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-07 04:25:22 -0400

    Palindrome Successful

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //remember the replace function
+    str = str.replace(/[\W_]/g, '').toLowerCase();
+    const regular = [...str].join('');
+    const reverse = [...str].reverse().join('');
+    return(regular === reverse);
 };
-
+console.log(palindromes('racecar'));
 // Do not edit below this line
 module.exports = palindromes;
