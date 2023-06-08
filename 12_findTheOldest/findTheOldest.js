7b2b0ee011bdc76d3a1cdbabdd84ad84b74a19d2
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-07 22:03:54 -0400

    Finished fundamentals part 5

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentDate = new Date().getFullYear();
+    const oldest = array.reduce((accumulator, cVal)=>{
+        const accumulatorAge = accumulator.yearOfDeath ? accumulator.yearOfDeath-accumulator.yearOfBirth : currentDate-accumulator.yearOfBirth;
+        const cValAge = cVal.yearOfDeath ? cVal.yearOfDeath - cVal.yearOfBirth : currentDate - cVal.yearOfBirth;
 
+        return accumulatorAge > cValAge ? accumulator : cVal;
+        
+    })
+    return oldest;
 };
 
+  
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+  ]
+  console.log(findTheOldest(people));
+  
 // Do not edit below this line
 module.exports = findTheOldest;
